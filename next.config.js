e7d2f6ae4f795c5c82a66bde373ceb360ae5c95f
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-06-15 22:22:54 +0530

    node-server

--- next.config.js
+++ next.config.js
@@ -4,14 +4,9 @@ const assetPrefix = isProd
   ? "https://victoria-five.vercel.app"
   : "http://localhost:3000";
 
-// const baseApiServer = isProd
-//   ? "https://nodappserver.herokuapp.com"
-//   : "http://localhost:8080";
-
 module.exports = {
   env: {
     PUBLIC_URL: "",
     baseUrl: assetPrefix,
-    API_SERVER: "https://nodappserver.herokuapp.com",
   },
 };
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,11 +1,28 @@
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
-export default function Photos({ data }) {
-  const posts = data.result;
-  return (
+export default function Photos() {
+  const [postDetail, setPostDetail] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // https://nodappserver.herokuapp.com/api/upload
+  https: useEffect(async () => {
+    const res = await fetch("https://nodappserver.herokuapp.com/api/upload");
+    const data = await res.json();
+    if (data.msg === "success") {
+      setLoading(false);
+      setPostDetail(data.result);
+    }
+  }, []);
+
+  // const res = await fetch("https://nodappserver.herokuapp.com/api/upload");
+  // const data = await res.json();
+  // const posts = data.result;
+  return loading ? (
+    <h1>Loading</h1>
+  ) : (
     <>
       <div style={{ display: "flex" }}>
-        {posts.map((post) => (
+        {postDetail.map((post) => (
           <div key={post.id} style={{ margin: 10 }}>
             <Image src={post.image} alt={post.title} width={250} height={300} />
           </div>
@@ -15,9 +32,9 @@ export default function Photos({ data }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch("https://nodappserver.herokuapp.com/api/upload");
-  const result = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch("https://nodappserver.herokuapp.com/api/upload");
+//   const result = await res.json();
 
-  return { props: { data: result } };
-}
+//   return { props: { data: result } };
+// }
