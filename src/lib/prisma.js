fe6f141d6127ffa8443c165f691341ac15be0993
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-06-24 19:19:00 +0530

    shop-integration

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+// import { PrismaClient } from "@prisma/client"
 
-  prisma = global.prisma;
-}
+// let prisma
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+
+//   prisma = global.prisma
+// }
 
 export default prisma;
