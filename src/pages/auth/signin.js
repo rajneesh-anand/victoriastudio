839972d8d1adef0378ce2dc3d1b3ff8215885ab4
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-06-30 00:21:29 +0530

    signIn-redirect

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { signIn, getCsrfToken, getSession } from "next-auth/client";
+import { signIn, getCsrfToken, getSession, useSession } from "next-auth/client";
 import SEO from "../../components/seo";
 import Footer from "../../layouts/footer";
 import Header from "../../layouts/header";
@@ -7,6 +7,7 @@ import Layout from "../../layouts";
 
 export default function SignIn({ csrfToken }) {
   const [email, setEmail] = useState("");
+  const [session, loading] = useSession();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,98 +15,100 @@ export default function SignIn({ csrfToken }) {
   };
 
   return (
-    <Layout>
-      <SEO
-        title="Login | Victoria Studio"
-        canonical={`${process.env.NEXTAUTH_URL}/auth/signin`}
-      />
-      <div className="wrapper about-page-wrapper">
-        <Header classOption="hb-border" />
-        <div className="main-content">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-md-6 col-lg-6">
-                <div className="signBlock">
-                  <div className="commonStyle">
-                    <p>SignIn Victoria Studio</p>
-                  </div>
-
-                  <div className="commonStyle">
-                    <button
-                      className="google"
-                      onClick={() =>
-                        signIn("google", {
-                          callbackUrl: "https://vic.vercel.app/",
-                        })
-                      }
-                    >
-                      <span
-                        className="fab fa-google fa-lg"
-                        aria-hidden="true"
-                      ></span>{" "}
-                      Login with Google
-                    </button>
-                  </div>
-
-                  <div className="commonStyle">
-                    <button
-                      className="facebook"
-                      onClick={() =>
-                        signIn("facebook", {
-                          callbackUrl: "https://vic.vercel.app/",
-                        })
-                      }
-                    >
-                      <span
-                        className="fab fa-facebook fa-lg"
-                        aria-hidden="true"
-                      ></span>{" "}
-                      Login with Facebook
-                    </button>
-                  </div>
-
-                  <form onSubmit={handleSubmit}>
-                    <input
-                      name="csrfToken"
-                      type="hidden"
-                      defaultValue={csrfToken}
-                    />
+    !loading && (
+      <Layout>
+        <SEO
+          title="Login | Victoria Studio"
+          canonical={`${process.env.NEXTAUTH_URL}/auth/signin`}
+        />
+        <div className="wrapper about-page-wrapper">
+          <Header classOption="hb-border" />
+          <div className="main-content">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-6">
+                  <div className="signBlock">
                     <div className="commonStyle">
-                      <input
-                        type="email"
-                        name="email"
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Enter your email !"
-                        value={email}
-                      />
+                      <p>SignIn Victoria Studio</p>
                     </div>
+
                     <div className="commonStyle">
-                      <button className="email" type="submit">
-                        Login With Email
+                      <button
+                        className="google"
+                        onClick={() =>
+                          signIn("google", {
+                            callbackUrl: "https://vic.vercel.app",
+                          })
+                        }
+                      >
+                        <span
+                          className="fab fa-google fa-lg"
+                          aria-hidden="true"
+                        ></span>{" "}
+                        Login with Google
                       </button>
                     </div>
-                  </form>
-                  <div>
-                    <p>
-                      By Login, you agree to Victoria Studio
-                      <a href="http://www.google.com"> Terms of Service </a>
-                      and
-                      <a target="_blank" href="http://www.google.com">
-                        {" "}
-                        Privacy Policy
-                      </a>
-                    </p>
+
+                    <div className="commonStyle">
+                      <button
+                        className="facebook"
+                        onClick={() =>
+                          signIn("facebook", {
+                            callbackUrl: "https://vic.vercel.app",
+                          })
+                        }
+                      >
+                        <span
+                          className="fab fa-facebook fa-lg"
+                          aria-hidden="true"
+                        ></span>{" "}
+                        Login with Facebook
+                      </button>
+                    </div>
+
+                    <form onSubmit={handleSubmit}>
+                      <input
+                        name="csrfToken"
+                        type="hidden"
+                        defaultValue={csrfToken}
+                      />
+                      <div className="commonStyle">
+                        <input
+                          type="email"
+                          name="email"
+                          required
+                          onChange={(e) => setEmail(e.target.value)}
+                          placeholder="Enter your email !"
+                          value={email}
+                        />
+                      </div>
+                      <div className="commonStyle">
+                        <button className="email" type="submit">
+                          Login With Email
+                        </button>
+                      </div>
+                    </form>
+                    <div>
+                      <p>
+                        By Login, you agree to Victoria Studio
+                        <a href="http://www.google.com"> Terms of Service </a>
+                        and
+                        <a target="_blank" href="http://www.google.com">
+                          {" "}
+                          Privacy Policy
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <Footer />
-      </div>
-    </Layout>
+          <Footer />
+        </div>
+      </Layout>
+    )
   );
 }
 
