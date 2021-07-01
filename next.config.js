36773ee4bc794cf2425639c574458d2576407128
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-07-01 23:10:01 +0530

    signin-check-6

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 const assetPrefix = isProd ? "https://vic.vercel.app" : "http://localhost:3000";
 
 module.exports = {
+  experimental: { css: true },
   env: {
     PUBLIC_URL: "",
     baseUrl: assetPrefix,
